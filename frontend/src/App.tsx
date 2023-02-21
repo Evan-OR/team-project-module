dd34af2b57c139e981d33cb8473f174543397228
Kevin <kevinglennon01@gmail.com>
2023-02-21 16:00:46 +0000

    adding nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       {/* //!Your code goes here bois */}
       <Navbar />
+      <div>cock</div>
       <h1>TEAM PROJECT</h1>
       <Hello />
     </div>
