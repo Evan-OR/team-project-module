ef344cf53f8907244f2adc31c760d1013dfd90a9
Kevin <kevinglennon01@gmail.com>
2023-02-22 00:58:36 +0000

    Added hero page and basic style to it & also have colour templated copied into all style sheets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar.js';
+import Hero from './components/Hero.js';
 
 function App() {
   return (
     <div className="App">
       {/* Your code goes here bois */}
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import heroStyles from '../styles/heroStyles.module.scss'
+
+const Hero = () => {
+    return(
+    <div className={heroStyles.Container}>
+        <div className={heroStyles.paragraphContainer}>
+        <h1 className={heroStyles.Header}>About us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea distinctio tempora dolor eaque, delectus corrupti, obcaecati mollitia voluptates expedita fuga tempore, officia beatae explicabo! Amet molestias incidunt vel debitis exercitationem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea distinctio tempora dolor eaque, delectus corrupti, obcaecati mollitia voluptates expedita fuga tempore, officia beatae explicabo! Amet molestias incidunt vel debitis exercitationem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea distinctio tempora dolor eaque, delectus corrupti, obcaecati mollitia voluptates expedita fuga tempore, officia beatae explicabo! Amet molestias incidunt vel debitis exercitationem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea distinctio tempora dolor eaque, delectus corrupti, obcaecati mollitia voluptates expedita fuga tempore, officia beatae explicabo! Amet molestias incidunt vel debitis exercitationem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea distinctio tempora dolor eaque, delectus corrupti, obcaecati mollitia voluptates expedita fuga tempore, officia beatae explicabo! Amet molestias incidunt vel debitis exercitationem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea distinctio tempora dolor eaque, delectus corrupti, obcaecati mollitia voluptates expedita fuga tempore, officia beatae explicabo! Amet molestias incidunt vel debitis exercitationem?</p>
+        </div>
+    </div>
+    )
+}
+
+export default Hero
