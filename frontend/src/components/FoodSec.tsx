f6e607c9254ebb15d36f5d9163da11d08b298d34
Kevin <kevinglennon01@gmail.com>
2023-02-25 08:00:48 +0000

    Reworded parts

--- frontend/src/components/FoodSec.tsx
+++ frontend/src/components/FoodSec.tsx
@@ -6,27 +6,27 @@ const DrinkSec = () => {
       <form action="" className={foodsecStyles.foodSearchbar}>
         <input
           type="text"
-          placeholder="Search for drink!"
+          placeholder="Search for Food!"
           className={foodsecStyles.foodInput}
         />
       </form>
 
       <div className={foodsecStyles.foodMenuContainer}>
         <div className={foodsecStyles.titleWrapper}>
-          <h2 className={foodsecStyles.title}>Suggested Drinks</h2>
+          <h2 className={foodsecStyles.title}>Suggested Food</h2>
         </div>
         <div className={foodsecStyles.foodCardWrapper}>
           <div className={foodsecStyles.foodItem}>
-            <h3 className={foodsecStyles.foodTitle}>Drink Item</h3>
+            <h3 className={foodsecStyles.foodTitle}>Food Item</h3>
           </div>
           <div className={foodsecStyles.foodItem}>
-            <h3 className={foodsecStyles.foodTitle}>Drink Item</h3>
+            <h3 className={foodsecStyles.foodTitle}>Food Item</h3>
           </div>
           <div className={foodsecStyles.foodItem}>
-            <h3 className={foodsecStyles.drinkTitle}>Drink Item</h3>
+            <h3 className={foodsecStyles.drinkTitle}>Food Item</h3>
           </div>
           <div className={foodsecStyles.foodItem}>
-            <h3 className={foodsecStyles.foodTitle}>Drink Item</h3>
+            <h3 className={foodsecStyles.foodTitle}>Food Item</h3>
           </div>
         </div>
       </div>
