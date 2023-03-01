9250a19653b07ba19418a0f71a8dbe560dcca6f6
18EOR <42158152+18EOR@users.noreply.github.com>
2023-03-01 18:34:19 +0000

    re-designed drink cards and page. Added new icons

---
+++ frontend/src/components/DrinksPage/DrinkCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import img from '../../images/drinkImgs/sexonthebeach.jpg';
+import styles from '../../styles/drinkCardStyle.module.scss';
+import { Drink } from '../../types/UserTypes';
+import FilledStar from '../icons/FilledStar';
+import HollowStar from '../icons/HollowStar';
+
+type DrinkCardProps = {
+  drink: Drink;
+};
+
+function DrinkCard(props: DrinkCardProps) {
+  const { drink } = props;
+
+  return (
+    <div className={styles.drinkCardWrapper2}>
+      <img draggable="false" src={img}></img>
+      <div className={styles.infoWrapper}>
+        <div className={styles.title}>{drink.strDrink}</div>
+        <div className={styles.tagWrapper}>
+          {drink.strTags?.split(',').map((tag) => (
+            <div key={tag} className={styles.tag}>
+              {tag}
+            </div>
+          ))}
+        </div>
+        <div className={styles.ratingWrapper}>
+          <FilledStar styles={`${styles.filledStar}`} />
+          <FilledStar styles={`${styles.filledStar}`} />
+          <FilledStar styles={`${styles.filledStar}`} />
+          <HollowStar styles={`${styles.hollowStar}`} />
+          <HollowStar styles={`${styles.hollowStar}`} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DrinkCard;
--- frontend/src/components/DrinksPage/DrinkSec.tsx
+++ frontend/src/components/DrinksPage/DrinkSec.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-import drinksecStyles from "../../styles/drinksecStyles.module.scss";
-import DrinkCards from "./DrinkCards";
+import React from 'react';
+import drinksecStyles from '../../styles/drinksecStyles.module.scss';
+import DrinkCard from './DrinkCard';
+import DrinkCards from './DrinkCards';
+import drinks from '../../dataset/drinks.json';
 
 const DrinkSec = () => {
   return (
     <div className={drinksecStyles.DrinkDisplayWrapper}>
       <form action="" className={drinksecStyles.Searchbar}>
-        <input
-          type="text"
-          placeholder="Search for drink!"
-          className={drinksecStyles.drinkInput}
-        />
+        <input type="text" placeholder="Search for drink!" className={drinksecStyles.drinkInput} />
       </form>
       <div className={drinksecStyles.DrinkMenuContainer}>
         <div className={drinksecStyles.titleWrapper}>
           <h2 className={drinksecStyles.title}>Suggested Drinks</h2>
         </div>
-        <DrinkCards />
+        <div className={drinksecStyles.cardDisplayWrapper}>
+          {drinks.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))}
+        </div>
       </div>
     </div>
   );
