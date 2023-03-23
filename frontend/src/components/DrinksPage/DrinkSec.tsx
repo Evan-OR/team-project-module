60550062b8037ae5edae084e2525356f9de18f1e
18EOR <42158152+18EOR@users.noreply.github.com>
2023-03-23 02:04:06 +0000

    recommended drinks now displaying in ui & drinks with similarity score of 0 dont get returned

--- frontend/src/components/DrinksPage/DrinkSec.tsx
+++ frontend/src/components/DrinksPage/DrinkSec.tsx
@@ -1,15 +1,17 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import drinksecStyles from '../../styles/drinksecStyles.module.scss';
 import DrinkCard from './DrinkCard';
 import drinks from '../../dataset/drinks.json';
 import DrinkModal from './DrinkModal';
 import { UserContext } from '../context/UserContext';
 import { getDrinkRecommendations } from '../../utils/drinksUtil';
+import { Drink } from '../../types/UserTypes';
 
 const DrinkSec = () => {
   const [modalToggle, setToggleModal] = useState(false);
   const [currentDrink, setCurrentDrink] = useState(drinks[0]);
   const userContext = useContext(UserContext);
+  const [drinkRecommendations, setDrinkRecommendations] = useState<Drink[]>([]);
 
   const toggleModal = (index: number | null) => {
     setToggleModal(!modalToggle);
@@ -18,10 +20,15 @@ const DrinkSec = () => {
     setCurrentDrink(drinks[index]);
   };
 
-  (() => {
-    if (userContext === null || userContext.user === null) return;
-    getDrinkRecommendations(drinks, userContext?.user?.likes);
-  })();
+  const initializeDrinkRecommendations = (): Drink[] => {
+    if (userContext === null || userContext.user === null) return [];
+    const dr = getDrinkRecommendations(drinks, userContext.user.likes);
+    return dr.length > 5 ? dr.slice(0, 4) : dr;
+  };
+
+  useEffect(() => {
+    setDrinkRecommendations(initializeDrinkRecommendations());
+  }, [userContext]);
 
   return (
     <div className={drinksecStyles.DrinkDisplayWrapper}>
@@ -30,9 +37,27 @@ const DrinkSec = () => {
         <input type="text" placeholder="Search for drink!" className={drinksecStyles.drinkInput} />
       </form>
 
+      {/* RENDER DRINK RECOMMENDATIONS START*/}
+      {drinkRecommendations.length > 0 ? (
+        <div className={drinksecStyles.DrinkMenuContainer}>
+          <div className={drinksecStyles.titleWrapper}>
+            <h2 className={drinksecStyles.title}>Suggested Drinks</h2>
+          </div>
+
+          <div className={drinksecStyles.cardDisplayWrapper}>
+            {drinkRecommendations.map((drink, index) => (
+              <DrinkCard key={drink.idDrink} drink={drink} toggleModal={toggleModal} index={index} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      {/* RENDER DRINK RECOMMENDATIONS END*/}
+
       <div className={drinksecStyles.DrinkMenuContainer}>
         <div className={drinksecStyles.titleWrapper}>
-          <h2 className={drinksecStyles.title}>Suggested Drinks</h2>
+          <h2 className={drinksecStyles.title}>Other Drinks</h2>
         </div>
 
         <div className={drinksecStyles.cardDisplayWrapper}>
