3f1f802f4703d03b74bc158804d664ee87cf9a5e
Kevin <kevinglennon01@gmail.com>
2023-04-01 23:33:19 +0100

    Fixed FoodSec file to work again after merge failed

--- frontend/src/components/FoodPage/FoodSec.tsx
+++ frontend/src/components/FoodPage/FoodSec.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import foodsecStyles from "../../styles/foodsecStyles.module.scss";
-import FoodCards from "./FoodCard";
-const foodSec = () => {
-  return (
-    <div className={foodsecStyles.foodDisplayWrapper}>
-      <form action="" className={foodsecStyles.Searchbar}>
-        <input
-          type="text"
-          placeholder="Search for Food!"
-          className={foodsecStyles.foodInput}
-        />
-      </form>
+import React, { Component } from "react";
+import { useState } from "react";
+import foodStyles from "../../styles/foodsecStyles.module.scss";
+import FoodCard from "./FoodCard";
+import food from "../../dataset/food.json"
+import { Meal } from "../../types/UserTypes";
 
-      <div className={foodsecStyles.foodMenuContainer}>
-        <div className={foodsecStyles.titleWrapper}>
-          <h2 className={foodsecStyles.title}>Suggested Food</h2>
+const foodSec = () => {
+  const [foodList, setFoodList] = useState<Meal[]>(food);
+
+  return(
+    <div className={foodStyles.FoodDisplayWrapper}>
+      <div className={foodStyles.titleWrapper}>
+          <h3 className={foodStyles.title}>Other Drinks</h3>
+      </div>
+      <div className={foodStyles.FoodMenuContainer}>
+        <div className={foodStyles.cardDisplayWrapper}>
+          {foodList.map((meal) => (
+          <FoodCard key={meal.idMeal} meal={meal}/> 
+          ))}
         </div>
-        <FoodCards />
       </div>
     </div>
-  );
+  )
 };
 
-export default foodSec;
+export default foodSec;  
