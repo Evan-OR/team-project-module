6013964e97962bd3989a9226535395c03f2dc599
Kevin <kevinglennon01@gmail.com>
2023-04-01 02:53:37 +0100

    Added the same styles from the drink page to the food section and added more meals to the json file

--- frontend/src/components/FoodPage/FoodSec.tsx
+++ frontend/src/components/FoodPage/FoodSec.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { useState } from "react";
-import foodsecStyles from "../../styles/foodsecStyles.module.scss";
+import foodStyles from "../../styles/foodsecStyles.module.scss";
 import FoodCard from "./FoodCard";
 import food from "../../dataset/food.json"
 import { Meal } from "../../types/UserTypes";
@@ -12,19 +12,19 @@ const foodSec = () => {
 
 
   return(
-    <div className="FoodWrapper">
-      {foodList.map((meal) => (
-        <FoodCard key={meal.idMeal} meal={meal}/> 
-        ))}
+    <div className={foodStyles.FoodDisplayWrapper}>
+      <div className={foodStyles.titleWrapper}>
+          <h3 className={foodStyles.title}>Other Drinks</h3>
+      </div>
+      <div className={foodStyles.FoodMenuContainer}>
+        <div className={foodStyles.cardDisplayWrapper}>
+          {foodList.map((meal) => (
+          <FoodCard key={meal.idMeal} meal={meal}/> 
+          ))}
+        </div>
+      </div>
     </div>
   )
 };
 
 export default foodSec;  
-
-//!This code words with getting out data from json
-  // const [meals, setMeals] = useState([
-  //       food.map((meals) => (
-  //         {mealName: meals.strMeal, mealID: meals.idMeal}
-  //       ))
-  //   ]);
