c4e141c20f779c5df318ffca1beded970f8f0722
Kevin <kevinglennon01@gmail.com>
2023-04-01 23:34:11 +0100

    Fixed FoodCard after merge fail

--- frontend/src/components/FoodPage/FoodCard.tsx
+++ frontend/src/components/FoodPage/FoodCard.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import foodData from '../../dataset/food.json';
-import foodcardStyles from '../../styles/foodCardStyle.module.scss';
-import sotb from '../../images/drinkImgs/sexonthebeach.jpg';
+import foodStyles from '../../styles/foodCardStyle.module.scss';
+import { Meal } from '../../types/UserTypes';
 
-export default function DrinkCards() {
+type foodCardProps = {
+  meal: Meal;
+}
+const FoodCard = (props: foodCardProps) => {
+  const{meal} = props;
   return (
-    <div className={foodcardStyles.foodMenu}>
-      {/* {foodData.map((food) => {
-        return (
-          <div className={foodcardStyles.foodCardWrapper} key={food.id}>
-            <div className={foodcardStyles.foodCard}>
-              <div className={foodcardStyles.foodItem} key={food.id}>
-                <img src={sotb} alt="sotbimg" className={foodcardStyles.foodImg} />
-                <h3 className={foodcardStyles.foodTitle}>{food.name}</h3>
-                <div className={foodcardStyles.foodSmallInfoWrapper}>
-                  <div className={foodcardStyles.foodSmallInfo}>
-                    <button className={foodcardStyles.foodMoreInfoBtn}>Read More</button>
-                    <p className={foodcardStyles.foodRating}>Rating 5/5</p>
-                  </div>
-                </div>
-              </div>
+    <div className={foodStyles.foodCardWrapper}>
+      <img draggable="false" src={meal.strMealThumb}/>
+      <div className={foodStyles.infoWrapper}>
+        <div className={foodStyles.title}>
+          {meal.strMeal}
+        </div>
+        <div className={foodStyles.tagWrapper}>
+        {meal.strTags?.split(',').map((tag) => (
+            <div key={tag} className={foodStyles.tag}>
+              {tag}
             </div>
-          </div>
-        );
-      })} */}
+          ))}
+        </div>
+      </div>
     </div>
   );
-  //! TODO: NEED TO ADD MODAL TO GIVE FULL INSTRUCTIONS ON HOW TO MAKE THE DRINK
-  //! AND COMMENT SECTION ETC...
 }
+
+export default FoodCard
